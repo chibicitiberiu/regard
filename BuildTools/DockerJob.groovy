0e48747645a13bb1095713b43aa7383c894749f2
Tiberiu Chibici <chibicitiberiu@gmail.com>
2021-03-28 01:11:59 +0200

    Remove pushing build tag to dockerhub

--- BuildTools/DockerJob.groovy
+++ BuildTools/DockerJob.groovy
@@ -4,28 +4,24 @@ node {
         stage('Backend (release)') {
             def dockerfile = 'Dockerfile.Backend'
             def img = docker.build("chibicitiberiu/regard-backend:${env.BUILD_ID}", "-f ${dockerfile} --build-arg CONFIG=Release .")
-            img.push()
             img.push('latest')
         }
         
         stage('Frontend (release)') {
             def dockerfile = 'Dockerfile.Frontend'
             def img = docker.build("chibicitiberiu/regard-frontend:${env.BUILD_ID}", "-f ${dockerfile} --build-arg CONFIG=Release .")
-            img.push()
             img.push('latest')
         }
 
         stage('Backend (debug)') {
             def dockerfile = 'Dockerfile.Backend'
             def img = docker.build("chibicitiberiu/regard-backend:${env.BUILD_ID}-debug", "-f ${dockerfile} --build-arg CONFIG=Debug .")
-            img.push()
             img.push('latest-debug')
         }
         
         stage('Frontend (debug)') {
             def dockerfile = 'Dockerfile.Frontend'
             def img = docker.build("chibicitiberiu/regard-frontend:${env.BUILD_ID}-debug", "-f ${dockerfile} --build-arg CONFIG=Debug .")
-            img.push()
             img.push('latest-debug')
         }
     }
