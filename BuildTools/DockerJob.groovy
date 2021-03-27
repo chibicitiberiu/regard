509a69b2c83d9a8c647d1ca607b69096276ddac4
Tiberiu Chibici <chibicitiberiu@gmail.com>
2021-03-27 12:57:46 +0200

    Try to fix jenkins job

--- BuildTools/DockerJob.groovy
+++ BuildTools/DockerJob.groovy
@@ -1,16 +1,16 @@
 
 node {
     stage('Backend') {
-        steps {
-            def dockerfile = 'Dockerfile.Backend'
-            def customImage = docker.build("regard-backend:${env.BUILD_ID}", "-f ${dockerfile} .")
-        }
+
+        def dockerfile = 'Dockerfile.Backend'
+        def customImage = docker.build("regard-backend:${env.BUILD_ID}", "-f ${dockerfile} .")
+
     }
     
     stage('Frontend') {
-        steps {
-            def dockerfile = 'Dockerfile.Frontend'
-            def customImage = docker.build("regard-frontend:${env.BUILD_ID}", "-f ${dockerfile} .")
-        }
+
+        def dockerfile = 'Dockerfile.Frontend'
+        def customImage = docker.build("regard-frontend:${env.BUILD_ID}", "-f ${dockerfile} .")
+
     }
 }
