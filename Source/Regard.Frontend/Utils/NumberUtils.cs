948aea23cecc2ded76b5e1983e9eaf2458dc1096
Tiberiu Chibici <chibicitiberiu@gmail.com>
2021-04-11 01:04:43 +0300

    Fix viewcount bug

--- Source/Regard.Frontend/Utils/NumberUtils.cs
+++ Source/Regard.Frontend/Utils/NumberUtils.cs
@@ -10,11 +10,15 @@ namespace Regard.Frontend.Utils
         public static string ToMetric(this ulong number, int decimals = 0)
         {
             decimal x = Convert.ToDecimal(number);
+
+            if (x > 1000000000)
+                return (x / 1000000).ToString($"F{decimals}", CultureInfo.InvariantCulture) + "B";
+
             if (x > 1000000)
                 return (x / 1000000).ToString($"F{decimals}", CultureInfo.InvariantCulture) + "M";
 
             else if (x > 1000)
-                return (x / 1000).ToString($"F{decimals}", CultureInfo.InvariantCulture) + "M";
+                return (x / 1000).ToString($"F{decimals}", CultureInfo.InvariantCulture) + "k";
 
             else return number.ToString();
         }
